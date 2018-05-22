afdc3abb6a8a5d9f658b035b7ab90536c5c179ba
Anthony O'Connor <anthonyoconnor@gmail.com>
2018-05-21 22:17:53 -0700

    Add basic layout with bindings

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.less']
 })
 export class AppComponent {
-  title = 'app';
+  city = 'Vancouver';
+  weather = 'Sunny';
+  temp = 23.0;
 }
