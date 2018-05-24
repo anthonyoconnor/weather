b975e68801ac720e8822fdc291001d5971eaccee
Anthony O'Connor <anthonyoconnor@gmail.com>
2018-05-23 21:46:49 -0700

    Add input box with binding to add-city component

--- src/app/add-city/add-city.component.ts
+++ src/app/add-city/add-city.component.ts
@@ -6,6 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./add-city.component.less']
 })
 export class AddCityComponent implements OnInit {
+  newCity: string;
 
   constructor() { }
 
